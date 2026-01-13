af88e2146d75b76b041d2b18502af10880b35ad5
AdamKarmelRozkosz <adamkarmelrozkosz@gmail.com>
2026-01-13 10:02:39 +0000

    fead: added basic firmware for the keyboard with vial nkro vial and tap dance support

---
+++ firmware/pleiades54_REV1/config.h
@@ -0,0 +1,9 @@
+#pragma once
+#undef SOFT_SERIAL_PIN
+#define SOFT_SERIAL_PIN D3
+#define SPLIT_USB_DETECT
+
+// vial ID
+#define VIAL_KEYBOARD_UID {0x8F, 0x39, 0x44, 0x50, 0x21, 0x56, 0x12, 0xFA}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
